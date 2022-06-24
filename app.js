f4dc0e23c24f916af71ca502255c6bbb717c4910
Vynnukh <bwhittingham213@gmail.com>
2022-06-24 13:36:41 +0100

    Dice Game V3

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const tPg = document.getElementById("TitlePage")
-const btn = document.getElementById("DieButton")
 const main = document.getElementById("primary")
 const value3 = document.getElementById("dF3")
 const value4 = document.getElementById("dF4")
@@ -10,6 +9,7 @@ const victory = document.getElementById("win")
 let currentRoll = Math.ceil(Math.random() * 6)
 let currentScore = 0
 let scoreDisplay = document.getElementById("score")
+let btn = document.getElementsByClassName("DieButton")
 
 const Die = {
     "./imgs/dice_face_1.png":1,
@@ -20,34 +20,84 @@ const Die = {
     "./imgs/dice_face_6.png":6
 }
 
-btn.addEventListener("click", () => {
-    currentRoll
+// ^ Still figuring out how this array can be implemented properly
+
+let DiceBtn = () => {
     if(currentRoll === 1 && currentScore <= 20) {
-        tPg.style.display = "none"
         loss.style.display = "flex"
-    } else if (currentRoll == 2 && currentScore <= 20) {
+        loss.style.justifyContent = "space-evenly"
         tPg.style.display = "none"
+        // main.style.display = "none"
+        // value3.style.display = "none"
+        // value4.style.display = "none"
+        // value5.style.display = "none"
+        // value6.style.display = "none"
+        
+    } else if (currentRoll == 2 && currentScore <= 20) {
         main.style.display = "flex"
+        tPg.style.display = "none"
+        // loss.style.display = "none"
+        // value3.style.display = "none"
+        // value4.style.display = "none"
+        // value5.style.display = "none"
+        // value6.style.display = "none"
+        
         return(currentScore + 2)
     } else if (currentRoll == 3 && currentScore <= 20) {
-        tPg.style.display = "none"
         value3.style.display = "flex"
+        value3.style.justifyContent = "space-evenly"
+        tPg.style.display = "none"
+        // main.style.display = "none"
+        // loss.style.display = "none"
+        // value4.style.display = "none"
+        // value5.style.display = "none"
+        // value6.style.display = "none"
+        
         return(currentScore + 3)
     } else if (currentRoll == 4 && currentScore <= 20) {
-        tPg.style.display = "none"
         value4.style.display = "flex"
+        value4.style.justifyContent = "space-evenly"
+        tPg.style.display = "none"
+        // main.style.display = "none"
+        // value3.style.display = "none"
+        // loss.style.display = "none"
+        // value5.style.display = "none"
+        // value6.style.display = "none"
+        
         return(currentScore + 4)
     } else if (currentRoll == 5 && currentScore <= 20) {
-        tPg.style.display = "none"
         value5.style.display = "flex"
+        value5.style.justifyContent = "space-evenly"
+        tPg.style.display = "none"
+        // main.style.display = "none"
+        // value3.style.display = "none"
+        // value4.style.display = "none"
+        // loss.style.display = "none"
+        // value6.style.display = "none"
+        
         return(currentScore + 5)
     } else if (currentRoll == 6 & currentScore <= 20) {
-        tPg.style.display = "none"
         value6.style.display = "flex"
+        value6.style.justifyContent = "space-evenly"
+        tPg.style.display = "none"
+        // main.style.display = "none"
+        // value3.style.display = "none"
+        // value4.style.display = "none"
+        // value5.style.display = "none"
+        // loss.style.display = "none"
+        
         return(currentScore + 6)
     } else if (currentScore >= 21) {
+        victory.style.display = "flex"
         tPg.style.display = "none"
         main.style.display = "none"
-        victory.style.display = "flex"
+        
     }
+}
+
+// ^ Had to comment out some of this because when I had the buttons working a bit they broke when this was inputted
+
+btn.addEventListener("click", () => {
+    currentRoll
+    DiceBtn()
 })
